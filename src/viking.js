139651a1f9b93a8c1b59ca76e09ce7b8a7be01fa
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-10-07 11:03:25 +0200

    Iteration0 complete

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,16 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking { }
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
